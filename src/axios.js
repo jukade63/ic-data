598936c175dbe1d48eebd7823c8da9393cca846c
jukade63 <jukade63@gmail.com>
2023-12-06 13:34:50 +0700

    rollback

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? `http://${window.location.host}/api`
+    ? "http://54.151.154.92/api"
     : "http://localhost:8080/api";
 
 export default axios.create({
