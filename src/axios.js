236333fb268e3910e4203c473eedbd01a5245b2f
jukade63 <jukade63@gmail.com>
2023-12-06 12:27:53 +0700

    app build

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "/api"
+    ? "http://54.151.154.92/api"
     : "http://localhost:8080/api";
 
 export default axios.create({
